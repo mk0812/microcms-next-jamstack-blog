0ae00e3c103cd9c2bf4c99df134c706a3cd82a0c
mk0812 <matsuoka812930@gmail.com>
2022-06-05 12:20:30 +0900

    category追加

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import Link from 'next/link'
 import { client } from '../libs/client'
 
-export default function Home({ blog }) {
+export default function Home({ blog, category }) {
   return (
     <div>
+      <ul>
+        {category.map((category) => (
+          <li key={category.id}>
+            <Link href={`/category/${category.id}`}>
+              <a>{category.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
       <ul>
         {blog.map((blog) => (
           <li key={blog.id} className="text-3xl font-bold underline">
@@ -18,11 +27,13 @@ export default function Home({ blog }) {
 }
 
 export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: 'blog' })
+  const blog = await client.get({ endpoint: 'blog' })
+  const category = await client.get({ endpoint: 'categories' })
 
   return {
     props: {
-      blog: data.contents,
+      blog: blog.contents,
+      category: category.contents,
     },
   };
 }
